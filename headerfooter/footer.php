f41409158a8aef6fea878e60499c2dce21134518
ArcadeK <131340382+ArcadeK1@users.noreply.github.com>
2024-09-11 16:34:54 +0500

    Add files via upload

---
+++ headerfooter/footer.php
@@ -0,0 +1,15 @@
+<footer class="footer">
+
+<ul>
+
+    <li>
+        <p>&copy; ГБУ "МФЦ", 2024</p>
+    </li>
+
+    <li>
+        <a href="https://www.mfc45.ru/" style="text-decoration: none;">
+            <p>Настоящий сайт профильной организации (mfc45.ru)</p>
+        </a>
+    </li>
+
+</footer>
---
+++ headerfooter/header.php
@@ -0,0 +1,35 @@
+<nav class="navbar">
+        <ul>
+            <li class="logo">
+                <a href="index.php">
+                    <img id="mfclogo" src = "media/img/logo.png" height="65" alt = "img">
+                </a>
+            </li>
+
+            <li>
+                <a href="#newssection">
+                    Новости
+                </a>
+            </li>
+
+            <li>
+                <a href="#aboutsection">
+                    О Многофункциональном центре
+                </a>
+            </li>
+
+
+            <li>
+                <a href="#servicecards">
+                    Услуги
+                </a>
+            </li>
+
+            <li>
+                <a href="#contactssection">
+                    Контакты
+                </a>
+            </li>
+
+        </ul>
+    </nav>
